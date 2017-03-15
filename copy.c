3442da781a06484d4149b9eb2a981cf4287d6979
AngelViolet <431000257@qq.com4>
2017-03-15 09:44:49 +0000

    new code

---
+++ copy.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+	char ch;
+	int ret;
+	
+
+	FILE *fp1;
+	
+	
+	
+	
+	fp1 = fopen("file1.txt","r");
+	FILE *fp2;
+	fp2 = fopen("f3.txt","w");
+	
+	
+	
+	
+	while(1)
+	{
+		ret = fscanf(fp1, "%c", &ch);
+		
+		if(ret<=0)
+		break;
+		fprintf(fp2, "%c", ch);
+	}
+	fclose(fp1);
+	fclose(fp2);
+	return 0;
+}
